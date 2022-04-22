52174e02db793613a2a198b54203e5e3c82dfc7b
Sosina Tatyana <fedorova_t95@mail.ru>
2022-04-22 15:09:21 +0300

    Initial Commit

---
+++ Simple2NetworkingSimple/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Simple2NetworkingSimple
+//
+//  Created by Tatiana Sosina on 22.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
